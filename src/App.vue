f32e0cbb217e2caa6ad59bc12c9657530b126d82
Lian Studer <lian.studer@hslu.ch>
2024-01-15 13:17:15 +0100

    remove solutions

--- src/App.vue
+++ src/App.vue
@@ -10,45 +10,43 @@ const messages = useCollection(query(messagesCollection, orderBy("timestamp")));
 const auth = useFirebaseAuth();
 
 // Aufgabe 7: Füge hier die Zustandsvariable zur Speicherung der Eingabe des Nachrichtenfelds hinzu
-const messageInput = ref("");
+
 
 async function sendMessage() {
   // Aufgabe 7: Füge hier den Code zum Senden der Nachricht hinzu
-  await addDoc(messagesCollection, {
-    text: messageInput.value,
-    author: currentUser.value.email,
-    timestamp: Timestamp.now()
-  });
 
-  messageInput.value = "";
+
+
+
+
 }
 
 function isMyMessage(author) {
   // Aufgabe 8: Füge hier den Code hinzu, der prüft, ob eine Nachricht von dir geschrieben wurde
-  return author === currentUser.value.email;
+
 }
 
 // Aufgabe 5: Füge hier die Zustandsvariable ein, welche die Account Infos des eingeloggten Users speichert
-const currentUser = ref();
+
 
 const credentials = reactive({
   email: "",
 
   // Aufgabe 5: Füge hier eine zweite Zustandseigenschaft zur Speicherung des Passworts ein
-  password: "",
+
 })
 
 
 async function loginUser() {
   // Aufgabe 5: Füge hier den Code zum Anmelden des Benutzers ein
-  const result = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
-  currentUser.value = result.user;
+
+
 }
 
 async function logoutUser() {
   // Aufgabe 9: Füge hier den Code zum Abmelden des Benutzers ein
-  await signOut(auth);
-  currentUser.value = undefined;
+
+
 }
 </script>
 
@@ -61,10 +59,7 @@ async function logoutUser() {
           :data-receiver="!isMyMessage(message.author)">
 
           <!-- Aufgabe 6: Füge hier den Code zum Anzeigen einer einzelnen Nachricht ein -->
-          <span>
-            {{ message.text }}<br />
-            <small>by {{ message.author }}</small>
-          </span>
+
 
         </li>
       </ul>
@@ -73,8 +68,6 @@ async function logoutUser() {
       <form @submit.prevent="sendMessage">
 
         <!-- Aufgabe 7: Füge hier den Code des Eingabefelds und Knopfs zum Senden einer Nachricht ein -->
-        <input type="text" name="message" id="message-input" v-model="messageInput">
-        <input type="submit" value="Senden">
 
       </form>
     </div>
@@ -86,8 +79,6 @@ async function logoutUser() {
     <input type="email" name="email" id="email" v-model="credentials.email">
 
     <!-- Aufgabe 5: Füge hier den Code für das Passwortfeld ein, analog des Emailfeldes -->
-    <label for="password">Passwort</label>
-    <input type="password" name="password" id="password" v-model="credentials.password">
 
 
     <input type="submit" value="Login">
